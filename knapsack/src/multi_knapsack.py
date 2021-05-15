dac225b345ef5be421403033983a71205625971f
Sanjay Khanna <sanjay.bxo@gmail.com>
2021-05-15 10:24:52 -0700

    fix logic for multi_knapsack

--- knapsack/src/multi_knapsack.py
+++ knapsack/src/multi_knapsack.py
@@ -44,7 +44,7 @@ def solve_knapsack(items, capacity):
       for r in range(len(capacity[first_fit])):
         remaining[first_fit + 1][r] -= items[i][2 + r]
       remaining[0] += items[i][1]
-      selected.append((first_fit, items[i]))
+      selected.append((first_fit, items[i][0]))
       continue
 
     # Does not fit
@@ -54,7 +54,7 @@ def solve_knapsack(items, capacity):
 
     # Find max of first i items with reduced capacity
     first_fit = check_fit(items[i], capacity)
-    while first_fit > 0:
+    while first_fit >= 0:
       too_big = False
       new_capacity = [[c for c in k] for k in capacity]
       for c in range(len(capacity[first_fit])):
@@ -67,37 +67,74 @@ def solve_knapsack(items, capacity):
         if first_fit + 1 >= len(capacity):
           # item i is too big, skip it
           break
-        next_fit = check_fit(items[i], capacity[first_fit:])
+        next_fit = check_fit(items[i], capacity[first_fit+1:])
         if next_fit < 0:
           break
-        first_fit += next_fit
+        first_fit += next_fit + 1
         continue
 
       new_selected, new_remaining = solve_knapsack(items[:i], new_capacity)
-      if new_remaining[0] + items[i][1] <= remaining[0]:
+      new_selected.append((first_fit, items[i][0]))
+      new_remaining[0] += items[i][1]
+      if new_remaining[0] <= remaining[0]:
         if first_fit + 1 >= len(capacity):
           # there is no benefit to include item i
           break
-        next_fit = check_fit(items[i], capacity[first_fit:])
+        next_fit = check_fit(items[i], capacity[first_fit+1:])
         if next_fit < 0:
           break
-        first_fit += next_fit
+        first_fit += next_fit + 1
         continue
 
       remaining = new_remaining
-      remaining[0] += items[i][1]
-      selected = new_selected + [(first_fit, items[i])]
-
+      selected = new_selected
+      break
 
   return (selected, remaining)
 
 
-
 def print_knapsack(sack):
-  return f'value = {sack[1][0]}, items = ' + str([(s[0], s[1][0]) for s in sack[0]])
+  return f'value = {sack[1][0]}, items = ' + str(
+    [(k, sorted([s[1] for s in sack[0] if s[0] == k]))
+         for k in range(len(sack[1])-1)])
 
 
 if __name__ == '__main__':
+  print(print_knapsack(solve_knapsack(
+    [('A', 1, 1), ('B', 6, 2), ('C', 10, 3), ('D', 16, 5)], [(7,)])))
+  print(print_knapsack(solve_knapsack(
+    [('A', 1, 1), ('B', 6, 2), ('C', 10, 3), ('D', 16, 5)], [(6,)])))
+  print(print_knapsack(solve_knapsack(
+    [('A', 1, 1, 3), ('B', 6, 2, 2), ('C', 10, 3, 5), ('D', 16, 5, 4)],
+    [(7, 7)])))
+
+  print(print_knapsack(solve_knapsack(
+    [('A', 2, 1, 20),
+     ('B', 2, 1, 25),
+     ('C', 3, 2, 30),
+     ('D', 2, 3, 35),
+     ('E', 5, 3, 40),
+     ('F', 6, 3, 40),
+     ('G', 2, 3, 45),
+     ('H', 5, 3, 45),
+     ('I', 7, 4, 50)],
+    [(20, 245)]
+    )))
+
+  print(print_knapsack(solve_knapsack(
+    [('A', 10, 2, 512, 10),
+     ('B', 20, 4, 256, 15),
+     ('C', 30, 8, 128, 20),
+     ('D', 40, 16, 64, 25),
+     ('E', 50, 32, 32, 30),
+     ('F', 60, 64, 16, 35),
+     ('G', 70, 128, 8, 40),
+     ('H', 80, 140, 4, 45),
+     ('I', 90, 160, 2, 50),
+     ('J', 100, 180, 1, 55)],
+    [(300, 300, 100)]
+    )))
+
   print(print_knapsack(solve_knapsack(
     [('A', 10, 2, 512, 10),
      ('B', 20, 4, 256, 15),
